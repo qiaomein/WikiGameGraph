c87dedcab5d9898ec78e2e5c2d2238649a893ff6
Jack Qiao <jackqiao2002@gmail.com>
2020-11-30 19:33:52 -0600

    fixed url error checking

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ if __name__ == '__main__':
     query2 = input('Enter destination Wiki query URL: ')
     if WIKI_PREFIX not in query1 or WIKI_PREFIX not in query2:
         print("Enter valid URLs!")
-    start = query1
-    end = query2
-    #create graph
-    wg = WikiGraph()
-    #run bfs
-    wg.wiki_bfs(start,end)
+    else:
+        start = query1
+        end = query2
+        #create graph
+        wg = WikiGraph()
+        #run bfs
+        wg.wiki_bfs(start,end)
